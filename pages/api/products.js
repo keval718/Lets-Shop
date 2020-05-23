e32f8829dac2982c830e6b95b2ea0c281a5f42f7
keval718 <kevalvirat@gmail.com>
2020-05-23 11:01:05 -0400

    database connected

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,4 +1,7 @@
 import products from '../../static/products.json'
+import connectDB from '../../utils/connectDb'
+import { connections } from 'mongoose'
+connectDB();
 export default (req,res)=>{
  res.status(200).json(products)
 
