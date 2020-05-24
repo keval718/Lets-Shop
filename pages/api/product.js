7800949cd63a16705af467132bc9f4f446be5a8a
keval718 <kevalvirat@gmail.com>
2020-05-24 14:04:28 -0400

    product delete functionality added

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,7 +1,32 @@
-import Productlist from '../../models/Product';
+import Product from '../../models/Product';
+import ProductList from '../../components/Index/ProductList';
+
 export default async (req,res)=>{
-    const {_id}=req.query
-    const response=await Productlist.findOne({_id})
-      res.status(200).json(response);
-    
+  switch(req.method)
+  {
+    case "GET": 
+    await handleGetRequest(req,res);
+    break;
+    case "DELETE":
+    await handleDeleteRequest(req,res);
+    break;
+    default:
+      res.status(405).send(`Method:${req.method} not allowed`)
+      break;
+
+  }
+}
+
+async function handleGetRequest(req,res){
+  const {_id}=req.query
+  const response=await Product.findOne({_id})
+  res.status(200).json(response);
+
+}
+
+async function handleDeleteRequest(req,res){
+  const {_id}=req.query
+   await Product.findOneAndDelete({_id})
+   res.status(204).json({})
+
 }
