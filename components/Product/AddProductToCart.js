d39f02ed854b64aa539b53842d01efcf5cdc671c
keval718 <kevalvirat@gmail.com>
2020-05-24 12:50:00 -0400

    default value for input added

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,7 +1,7 @@
 import {Input,Button,Icon} from 'semantic-ui-react'
 function AddProductToCart() {
   return <>
-  <Input  focus type="number" placeholder="Quantity" min="1"  
+  <Input  focus type="number" placeholder="Quantity" min="1"  value="1"
     action={{
       color:"red",
       content:"Add to cart",
