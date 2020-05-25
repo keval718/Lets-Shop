b7e4c5efff9966b2eb416dcc8532d6d309398ad5
keval718 <kevalvirat@gmail.com>
2020-05-24 23:02:33 -0400

    create product ui created

--- pages/create.js
+++ pages/create.js
@@ -1,5 +1,59 @@
+import {Form,Input,TextArea,Image,Button,Icon,Message,Header, FormField } from 'semantic-ui-react'
 function CreateProduct() {
-  return <>create</>;
+  return <>
+  <Header as="h2" block >
+    <Icon name="add" color="orange"/>
+      Create New Product
+    </Header>
+    <Form>
+      <Form.Group widths="equal">
+        <Form.Field 
+        control={Input}
+        name="name"
+        label="Name"
+        placeholder="Name"
+        />
+          <Form.Field 
+        control={Input}
+        name="price"
+        label="Price"
+        placeholder="Price"
+        min="0.00"
+        step="0.01"
+        type="number"
+        />
+          <Form.Field 
+        control={Input}
+        name="media"
+        label="media"
+        accept="image/*" 
+        type="file"
+        content="select image"
+
+       
+        />
+
+      </Form.Group>
+      <Form.Field 
+      control={TextArea}
+      name="description"
+      label="Description"
+      placeholder="Description"
+
+      
+      />
+      <Form.Field 
+      control={Button}
+      color="blue"
+      icon="pencil alternate"
+      content="submit"
+      type="submit"
+      
+      
+      />
+    </Form>
+
+  </>;
 }
 
 export default CreateProduct;
