6c9fa008b0f958445bf8abc637adef8236163675
keval718 <kevalvirat@gmail.com>
2020-06-30 19:11:17 -0400

    cart structure created

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,5 +1,19 @@
+import {Button,Segement,Divider, Segment} from 'semantic-ui-react'
 function CartSummary() {
-  return <>CartSummary</>;
+  return <>
+    <Divider/>
+    <Segment clearing size="large">
+      <strong>Sub Total:</strong>$0.00
+      <Button 
+      icon="cart"
+      color="teal"
+      floated="right"
+      content="checkout"
+      />
+    </Segment>
+
+  
+ </>
 }
 
 export default CartSummary;
