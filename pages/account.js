ded133d1c126a9af6450249c4a8fa6c0b560ff7f
keval718 <kevalvirat@gmail.com>
2020-07-12 22:52:35 -0400

    order history created

--- pages/account.js
+++ pages/account.js
@@ -1,8 +1,27 @@
 import AccountHeader from '../components/Account/AccountHeader';
-function Account() {
+import AccountOrders from '../components/Account/AccountOrders';
+import {parseCookies} from 'nookies'
+import baseUrl from '../utils/baseUrl'
+import axios from 'axios'
+function Account({user,orders}) {
   return <>
   
-<AccountHeader/>  </>;
+<AccountHeader {...user}/> 
+<AccountOrders orders={orders}/>
+ </>;
+}
+
+Account.getInitialProps=async ctx =>{
+  const {token}=parseCookies(ctx)
+  if(!token)
+  {
+    return{orders:[]}
+  }
+  const payload={headers:{Authorization:token}}
+  const url=`${baseUrl}/api/orders`
+  const response=await axios.get(url,payload)
+  return response.data
+
 }
 
 export default Account;
