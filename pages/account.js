e4ff5039aca12150fae7ad1739fbdbed10d32332
keval718 <kevalvirat@gmail.com>
2020-07-13 16:06:07 -0400

    user permission given

--- pages/account.js
+++ pages/account.js
@@ -1,27 +1,29 @@
 import AccountHeader from '../components/Account/AccountHeader';
 import AccountOrders from '../components/Account/AccountOrders';
-import {parseCookies} from 'nookies'
-import baseUrl from '../utils/baseUrl'
-import axios from 'axios'
+import AccountPermissions from '../components/Account/AccountPermissions';
+
+import {parseCookies} from 'nookies';
+import baseUrl from '../utils/baseUrl';
+import axios from 'axios';
+
 function Account({user,orders}) {
   return <>
-  
-<AccountHeader {...user}/> 
-<AccountOrders orders={orders}/>
- </>;
+    <AccountHeader {...user}/>
+    <AccountOrders orders={orders}/>
+    {user.role === 'root' && <AccountPermissions currentUserId={user._id}/>}
+  </>;
 }
 
-Account.getInitialProps=async ctx =>{
-  const {token}=parseCookies(ctx)
-  if(!token)
-  {
-    return{orders:[]}
+Account.getInitialProps = async (ctx)=>{
+  const {token} = parseCookies(ctx);
+  if(!token){
+    return {orders:[]}
   }
-  const payload={headers:{Authorization:token}}
-  const url=`${baseUrl}/api/orders`
-  const response=await axios.get(url,payload)
-  return response.data
 
+  const payload = {headers:{Authorization:token}}
+  const url = `${baseUrl}/api/orders`;
+
+  const response = await axios.get(url,payload);
+  return response.data;
 }
-
 export default Account;
